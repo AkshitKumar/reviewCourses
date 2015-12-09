9caa472a5af254b8a5823f58a8bf721aa9d5a313
AkshitKumar <akshitkumar100@gmail.com>
2015-12-09 17:07:11 +0530

    Add user permissions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [ :edit, :update, :destroy]
   before_action :set_course
   before_action :authenticate_user!
+  before_action :check_user, only: [:edit, :update , :destroy]
   # GET /reviews
   # GET /reviews.json
 
@@ -51,7 +52,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to course_path(@course), notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -64,6 +65,12 @@ class ReviewsController < ApplicationController
 
     def set_course
       @course = Course.find(params[:course_id])
+    end
+
+    def check_review
+      unless (@review.user == current_user) || (current_user.admin?)
+        redirect_to root_url , alert: "Sorry this review belongs to someone else"
+      end
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params

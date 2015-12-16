a6e1c67fcafc982bd535101c02cb947eed253b44
AkshitKumar <akshitkumar100@gmail.com>
2015-12-16 12:38:33 +0530

    Changed DB

--- db/schema.rb
+++ db/schema.rb
@@ -22,11 +22,6 @@ ActiveRecord::Schema.define(version: 20151209054638) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "oauth", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "oauths", force: :cascade do |t|
     t.string   "only"
     t.string   "create"

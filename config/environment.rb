a665ddc6fb0ee107d5e9fd15251e4ec3b5a4ca16
sourav27 <debnath.sourabh@gmail.com>
2016-01-31 10:40:26 +0530

    changed about

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'institutewebops@gmail.com',
-  :password => 'instiwmo1516',
+  :user_name => 'username@gmail.com',
+  :password => 'password',
   :domain => 'gmail.com',
   :address => 'smtp.gmail.com',
   :port => 587,
